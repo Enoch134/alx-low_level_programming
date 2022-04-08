9e14a22bc64d69d74a03a5be219684cfae1e1ac9
Enoch134 <men270992@gmail.com>
2022-04-08 15:36:51 -0700

    My print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -13,4 +13,5 @@ int main(void)
 		putchar(ch);
 		putchar('\n');
 	return (0);
-}
+}[A[A[A[A[A[A[A[A[A[C[C[C*/[A[C[C[C[C[C[C[C[C[*Re*[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[* main - print aiphabet in lowercase[B[B[B[B[B[B[B[B[B[D[D[D[D[D[D[D[D
+[B[B[C

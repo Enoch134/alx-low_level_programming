78d05bd13adc515d1dc718feb35900950269c0f8
Enoch134 <men270992@gmail.com>
2022-04-08 15:27:45 -0700

    My print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - print alphabet in lowercase
+*
+*/
+int main(void)
+{
+        char ch = 'a';
+ 
+        for(ch = 'a'; ch <= 'z'; ch++)
+              [B[B
+              putchar(ch)'
+              putchar('\n');[A[D[;[B[C[C
+[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[A[D[D
+[B        return (0);
+}

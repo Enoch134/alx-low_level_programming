0755d6692d4e3927609e7782536d663191b61062
Enoch134 <men270992@gmail.com>
2022-04-08 15:17:46 -0700

    My last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,37 +3,28 @@
 #include <stdio.h>
 
 /**
-* main -Checks last digit
-*
-[A[[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[C/**
-* main - Checks last digit
-[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[C[C[C/**
-* main - Checks last digit
-* Return: zero
-*/
-
-[Aint main(void)
+   * main - Checks last digit
+   *
+   *Return: zero
+   */
+int main(void)
 {
-     int n;
-     int 1;
+	int n;
+	int l;
 
-     srand(time(0));
-     n= rand() - RAND_MAX / 2
-     l = n % 10;
-     
-     if(l == 0)
-     {
-     printf("%[A[A[> 5[C[C[BLast digit of %d is %d  and is greater than 5\n", n, l[C)
-     }
-     [A}else if (l == 0)
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	l = n % 10;
 
-[A     {
-     printf(
-[A[C[C[C[C[C[C[C[C[C[C[C[C"Last digit of %d is %d and is 0\n", n,l);[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[;[B[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
-     }
-      [Aelse if (l < 6 && l !=0)
-     {
-      [A[A[A[D p[B[B[B[D[Dprintf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
-     }
-     return (0);
+	if (l > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
+	} else if (l == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, l);
+	} else if (l < 6 && l != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	}
+	return (0);
 }

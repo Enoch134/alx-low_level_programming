7fb6fa9d7082ed1cfbc8c9d8d2c4cc4e9999d131
Enoch134 <men270992@gmail.com>
2022-04-08 14:04:51 -0700

    My positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+   * main - Checks if positive, zero or negative
+   *
+   *Return: zero
+   */
+int main()[Dvoid)
+{
+        int n;
+        
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        if (n > 0)
+        (
+               printf("%d is positive\n", n)[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[{[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+         }else if (n == 0)
+         {
+               printf("%d is zero\n", n);[A[A[A[C[C[C;[B[B[B[B
+         }else
+         {
+               printf("%d is negative\n", n);
+         }
+         return (0);
+}
